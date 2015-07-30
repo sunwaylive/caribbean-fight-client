daa5b60ec9e0830705f03426e905094ddf39fd27
sunwei <sunwayliving@gmail.com>
2015-07-31 00:32:48 +0800

    added go back btn on PVP scene

--- src/PVPMainScene.lua
+++ src/PVPMainScene.lua
@@ -19,8 +19,9 @@ function PVPMainScene:createLayer()
     --create layer
     local layer = cc.Layer:create()
 
-    --添加 创建房间的按钮
+    --添加 创建房间,以及返回上一层的按钮
     self:addCreateRoomBtn(layer)
+    self:addBackBtn(layer)
 
     return layer
 end
@@ -30,13 +31,14 @@ function PVPMainScene:addCreateRoomBtn(layer)
     --step1: 添加 创建房间的按钮
     local isTouchButtonCreateRoom = false
     local button_callback_createroom = function(sender, eventType)
+        --确保这个按钮只被点击了一次
         if isTouchButtonCreateRoom == false then
             isTouchButtonCreateRoom = true
             if eventType == ccui.TouchEventType.began then
                 ccexp.AudioEngine:play2d(BGM_RES.MAINMENUSTART, false, 1)
                 ccexp.AudioEngine:stop(AUDIO_ID.MAINMENUBGM)
                 cclog("create room btn is clicked")
-                
+
             end
         end
     end
@@ -50,6 +52,31 @@ function PVPMainScene:addCreateRoomBtn(layer)
     local effectSpriteCreateRoom = cc.EffectSprite:create("mainmenuscene/start.png")
     effectSpriteCreateRoom:setPosition(self.size.width*0.5 + 100,self.size.height* 0.85)
     layer:addChild(effectSpriteCreateRoom,5)
+end
+
+function PVPMainScene:addBackBtn(layer)
+    local isTouchBtnBack = false
+    local btn_callback_back = function(sender, eventType)
+        if isTouchBtnBack == false then
+            isTouchBtnBack = true
+            if eventType == ccui.TouchEventType.began then
+                ccexp.AudioEngine:play2d(BGM_RES.MAINMENUSTART, false, 1)
+                ccexp.AudioEngine:stop(AUDIO_ID.MAINMENUBGM)
+                cclog("back btn is pressed!")
+                cc.Director:getInstance():replaceScene(require("MainMenuScene").create())
+            end
+        end
+    end
+
+    local btnBack = ccui.Button:create("start.png","","",ccui.TextureResType.plistType)
+    btnBack:setPosition(0 + 100 ,self.size.height * 0.85)
+    --用这种方式添加按钮响应函数
+    btnBack:addTouchEventListener(btn_callback_back)
+    layer:addChild(btnBack,4)
+
+    local effectSpriteBack = cc.EffectSprite:create("mainmenuscene/start.png")
+    effectSpriteBack:setPosition(0 + 100,self.size.height* 0.85)
+    layer:addChild(effectSpriteBack,5)
 end
 
 return PVPMainScene
