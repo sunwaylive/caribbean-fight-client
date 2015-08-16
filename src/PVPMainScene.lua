a30e05da76daa9be34246d18a14f894827d3101f
tengsh1313 <tengsh1313@gmail.com>
2015-08-16 21:47:52 +0800

    写了列出房间的方法。写了对服务器返回信息的分析处理。

--- src/PVPMainScene.lua
+++ src/PVPMainScene.lua
@@ -1,10 +1,16 @@
+require("Helper")
+
+local fontPath = "chooseRole/actor_param.ttf"
 --导入socket库
 local socket = require("socket") --如果不行换这个试试 require('socket.core');
 
 client_socket = nil --本客户端和服务器通信的tcp链接
 local label1
 local label2
+local roomList = List.new()
+local roomMenu
 local str = "0"
+LINE_SPACE = 80
 local num = 0
 
 local PVPMainScene  = class("PVPMainScene",function ()
@@ -20,34 +26,53 @@ end
 
 function PVPMainScene.create()
     local scene = PVPMainScene.new()
-    local layer = scene:createLayer()
+    layer = scene:createLayer()
     scene:addChild(layer)
-    
+
     return scene
 end
 
 local function tcpListener(dt)
 	local r = client_socket:receive('*l')
-	-- io.output("out.txt")
-	-- if r~= nil then
-		-- io.write(r)
-	-- else
-		-- io.write("nil\n")
-	-- end
-	-- io.output():close()
 	if r==nil then
-		--str = str + "nil"
-		--label1:setString("nil")
 		return
 	else
-		str = str.."x"..r
-		label1:setString(str)
-		--label1:setString(r)
+		label1:setString(r)
+		local i = string.find(r,'@')
+		if i == nil then 
+			return 
+		end
+		local rType = string.sub(r,1,i-1)
+		local rContent = string.sub(r,i+1,-1)
+		label2:setString(rType)
+		if rType == "createRoom" then
+			if rContent == "OK" then
+			
+			end
+		elseif rType == "listRoom" then
+			rooms = rContent
+			List.removeAll(roomList)
+			while(1)
+			do
+				local j = string.find(rooms,' ')
+				local k = string.find(rooms,'|')
+				local roomID = string.sub(rooms,1,j-1)
+				local playerNum = string.sub(rooms,j+1,k-1)
+				--把房间存入列表
+				List.pushlast(roomList,{roomID=roomID, playerNum=playerNum})
+				--如果后面没有其他房间了
+				if k == string.len(rooms) then
+					break
+				end
+				rooms = string.sub(rooms,k+1,-1)
+			end
+			PVPMainScene:addRoomLabel(PVPMainScene,roomList)
+		elseif rType == "joinRoom" then
+		
+		elseif rType == "startGame" then
+		
+		end
 	end
-end
-
-local function update(dt)
-	label2:setString(str..num)
 end
 
 function PVPMainScene:createLayer()
@@ -56,24 +81,88 @@ function PVPMainScene:createLayer()
 	self:addLabel(layer)
     --连接服务器
     self:connectToServer()
-    
     --创建UI元素
     self:addBackBtn(layer)
     self:addCreateRoomBtn(layer)
     self:addJoinRoomBtn(layer)
     self:addStartGameBtn(layer)
-    --TODO: 这边会导致界面阻塞
-	--listener = cc.Director:getInstance():getScheduler():scheduleScriptFunc(tcpListener, 0, false)
-	--scheduler = cc.Director:getInstance():getScheduler():scheduleScriptFunc(update, 0, false)
+	List.pushlast(roomList,{roomID=1002,playerNum=2})
+	List.pushlast(roomList,{roomID=1003,playerNum=3})
+	List.pushlast(roomList,{roomID=1004,playerNum=5})
+	self:addRoomLabel(layer, roomList)
+	listener = cc.Director:getInstance():getScheduler():scheduleScriptFunc(tcpListener, 0, false)
     return layer
+end
+
+function PVPMainScene:addRoomLabel(layer, list)
+	local function menuCallback(tag)
+        print(tag)
+        local Idx = tag - 10000
+        local roomID = roomList[Idx].roomID
+		self:joinRoom(roomID)
+    end
+	
+	local index
+	local menu = cc.Menu:create()
+	for index=list.first, list.last do
+		-- label、menuItem、menu的坐标都能影响最终的菜单位置
+		local label = cc.Label:createWithTTF("RoomID:"..list[index].roomID..string.rep(" ",10).."PlayerNum:"..list[index].playerNum,fontPath, 40)
+		label:setColor(cc.V3(255,0,0))
+		label:setAnchorPoint(cc.p(0.5,0.5))
+		local menuItem = cc.MenuItemLabel:create(label)
+		menuItem:setPosition(cc.p(self.size.width/2,self.size.height*0.7-index*LINE_SPACE))
+		menuItem:registerScriptTapHandler(menuCallback)
+		menu:addChild(menuItem, index+10000, index+10000)
+	end
+	menu:setPosition(0,0)
+	menu:setContentSize(cc.size(self.size.width, List.getSize(list)*LINE_SPACE))
+	layer:addChild(menu)
+	
+	-- handling touch events
+    local function onTouchBegan(touch, event)
+        BeginPos = touch:getLocation()
+        -- CCTOUCHBEGAN event must return true
+        return true
+    end
+
+    local function onTouchMoved(touch, event)
+        local location = touch:getLocation()
+        local nMoveY = location.y - BeginPos.y
+        local curPosx, curPosy = menu:getPosition()
+        local nextPosy = curPosy + nMoveY
+        local winSize = cc.Director:getInstance():getWinSize()
+        if nextPosy < 0 then
+            menu:setPosition(0, 0)
+            return
+        end
+		
+		if (List.getSize(roomList) + 1) * LINE_SPACE - winSize.height < 0 then
+			return
+		end
+
+        if nextPosy > ((List.getSize(roomList) + 1) * LINE_SPACE - winSize.height) then
+            menu:setPosition(0, ((List.getSize(roomList) + 1) * LINE_SPACE - winSize.height))
+            return
+        end
+
+        menu:setPosition(curPosx, nextPosy)
+        BeginPos = {x = location.x, y = location.y}
+        CurPos = {x = curPosx, y = nextPosy}
+    end
+
+    local listener = cc.EventListenerTouchOneByOne:create()
+    listener:registerScriptHandler(onTouchBegan,cc.Handler.EVENT_TOUCH_BEGAN )
+    listener:registerScriptHandler(onTouchMoved,cc.Handler.EVENT_TOUCH_MOVED )
+    local eventDispatcher = self:getEventDispatcher()
+    eventDispatcher:addEventListenerWithSceneGraphPriority(listener, self)
 end
 
 --pvp establish tcp connect
 function PVPMainScene:connectToServer()
     local server_ip = "112.74.199.45"
-    local server_port = 8383
+    local server_port = 2348
     client_socket = socket.tcp()
-    --client_socket:settimeout(5)
+    client_socket:settimeout(5)
     
     --In case of error, the method returns nil followed by a string describing the error. In case of success, the method returns 1.
     if client_socket:connect(server_ip, server_port) == 1 then
@@ -85,32 +174,44 @@ function PVPMainScene:connectToServer()
 end
 
 --pvp list room
-function PVPMainScene:listRoom()
-    
+function PVPMainScene:listRoom(roomList)
+	self.label:setString("listRoom")
+	local r, e = client_socket:send("listRoom\n")
+	print(r,e)
+	if e~= nil then
+		label1:setString(e)
+	end
 end
 
 --PVP create room
 function PVPMainScene:createRoom()
-	self.label:setString("createRoom"..num)
-	num = num + 1
-	print(num)
-	--label1:setString("createRoom")
-	--label1:setString(str)
+	self.label:setString("createRoom")
     local r, e = client_socket:send("createRoom 2\n") --第一个返回值是发送的字节数的意思, 第二个返回值是错误码，如果成功则返回nil
     --cclog(r)
 	print(r,e)
 	if e~= nil then
-		self.label:setString(e)
+		label1:setString(e)
 	end
 	print("OK")
 end
 
 --PVP join room
-function PVPMainScene:joinRoom()
-
+function PVPMainScene:joinRoom(roomID)
+	self.label:setString("joinRoom")
+	local r, e = client_socket:send("joinRoom "..roomID.."\n")
+	print(r,e)
+	if e~= nil then
+		label1:setString(e)
+	end
 end
 
 function PVPMainScene:startGame()
+	-- self.label:setString("startGame")
+	-- local r, e = client_socket:send("startGame "..roomID.."\n")
+	-- print(r,e)
+	-- if e~= nil then
+		-- label1:setString(e)
+	-- end
     --local scene = require("PVPBattleScene")
     --cc.Director:getInstance():replaceScene(scene.create())
     if client_socket ~= nil then
@@ -142,6 +243,8 @@ function PVPMainScene:addBackBtn(layer)
                 ccexp.AudioEngine:play2d(BGM_RES.MAINMENUSTART, false, 1)
                 ccexp.AudioEngine:stop(AUDIO_ID.MAINMENUBGM)
                 cclog("back btn is pressed!")
+				--清空房间列表
+				List.removeAll(roomList)
                 cc.Director:getInstance():replaceScene(require("MainMenuScene").create())
             end
         end
@@ -196,7 +299,7 @@ function PVPMainScene:addJoinRoomBtn(layer)
                 ccexp.AudioEngine:play2d(BGM_RES.MAINMENUSTART, false, 1)
                 ccexp.AudioEngine:stop(AUDIO_ID.MAINMENUBGM)
                 cclog("join room btn is clicked")
-                self:joinRoom()--加入PVP房间
+                --self:joinRoom(roomID)--加入PVP房间
             end
         end
     end
@@ -231,25 +334,27 @@ function PVPMainScene:addStartGameBtn(layer)
     btnStartGame:addTouchEventListener(button_callback_startgame)
     layer:addChild(btnStartGame, 5)
 end
-
 --用于显示调试信息
 function PVPMainScene:addLabel(layer)
-    self.label = cc.Label:createWithTTF("Hello World","chooseRole/actor_param.ttf", 20)
+    self.label = cc.Label:createWithTTF("Hello World",fontPath, 20)
     self.label:setPosition(self.size.width*0.5,self.size.height*0.5)
 	self.label:setColor(cc.V3(255,0,0))
 	--self.label:setVisible(false)
     self:addChild(self.label,1)
 	
-	label1 = cc.Label:createWithTTF("Hello World","chooseRole/actor_param.ttf", 20)
+	label1 = cc.Label:createWithTTF("Hello World",fontPath, 20)
     label1:setPosition(self.size.width*0.5,self.size.height*0.4)
 	label1:setColor(cc.V3(255,0,0))
+	print(label1:getPosition())
 	self:addChild(label1,1)
+	--label1:setVisible(false)
 	label1:setString("hello")
 	
-	label2 = cc.Label:createWithTTF("Hello World","chooseRole/actor_param.ttf", 20)
+	label2 = cc.Label:createWithTTF("Hello World",fontPath, 20)
     label2:setPosition(self.size.width*0.5,self.size.height*0.2)
 	label2:setColor(cc.V3(255,0,0))
 	self:addChild(label2,1)
+	--label2:setVisible(false)
 	label2:setString("hello")
 end
 
